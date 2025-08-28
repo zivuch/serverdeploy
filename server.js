31d539ed4d1f3a553ba4049368db3466a29cb10b
zivuch <zivuch@gmail.com>
2025-08-28 20:12:28 +0300

    first commit

--- server.js
+++ server.js
@@ -11,3 +11,8 @@ app.listen(PORT, () => console.log(`run on ${PORT}`));
 app.get("/test", (req, res) => {
   res.json({ msg: `hello from server, my name is ${process.env.NAME}` });
 });
+
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome ${process.env.NAME}</h1>`);
+});
