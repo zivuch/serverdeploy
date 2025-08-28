52fd8f9cd6eb6e90a85dd7e458a990ca5902e693
zivuch <zivuch@gmail.com>
2025-08-28 20:26:23 +0300

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
@@ -8,10 +9,11 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`run on ${PORT}`));
 
+console.error("Name=>", process.env.NAME);
+
 app.get("/test", (req, res) => {
   res.json({ msg: `hello from server, my name is ${process.env.NAME}` });
 });
-
 
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome ${process.env.NAME}</h1>`);
